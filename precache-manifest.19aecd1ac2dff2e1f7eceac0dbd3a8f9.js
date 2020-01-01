33e155636a8b72f3a9ab5b26ab55c86670f28cbe
David Akuma <chukay360@gmail.com>
2020-01-01 15:29:06 +0100

    Updates

---
+++ precache-manifest.19aecd1ac2dff2e1f7eceac0dbd3a8f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc689b6b7bfd9c736ff45406606b4369",
+    "url": "/customer-app/index.html"
+  },
+  {
+    "revision": "683ae405083cf5362a36",
+    "url": "/customer-app/static/css/main.50853cd7.chunk.css"
+  },
+  {
+    "revision": "22fc8ed508607bccd227",
+    "url": "/customer-app/static/js/2.9428489b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/customer-app/static/js/2.9428489b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "683ae405083cf5362a36",
+    "url": "/customer-app/static/js/main.45ddbb14.chunk.js"
+  },
+  {
+    "revision": "593d188af41c1f6a2fb5",
+    "url": "/customer-app/static/js/runtime-main.cf76f71b.js"
+  }
+]);
